9ad616a63d8804d1a49af6f7dacc0f5df6daaa47
chenxudong <chenxudong@didiglobal.com>
2019-02-19 16:54:55 +0800

    3

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-enum': [2, 'always', [
+      "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
+    ]],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
     'scope-empty': [2, 'never']
   }
 }
